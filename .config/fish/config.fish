c01bf2db6cb60f0bce9afd50f576b9466b35727b
davidshukhin <davidshukhin@gmail.com>
2025-03-08 14:46:01 -0500

    lazyvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,5 +8,9 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export QT_QPA_PLATFORM="wayland;xcb"
 set -x PATH $PATH /home/david/Android/Sdk/emulator
 set fish_greeting
+# Set JAVA_HOME
+set -x JAVA_HOME /usr/lib/jvm/default
+set -x PATH $PATH $JAVA_HOME/bin
 starship init fish | source
+zoxide init fish | source
 fastfetch
